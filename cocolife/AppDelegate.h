a3cbdda7856f878092f0e4bcbdd2f9f39a4e15c5
yu kawase <kawase@kawase-no-MacBook-Air.local>
2013-03-12 10:54:07 +0900

    Initial Commit

---
+++ cocolife/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cocolife
+//
+//  Created by yu kawase on 13/03/12.
+//  Copyright (c) 2013年 cocolife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

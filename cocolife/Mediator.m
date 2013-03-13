ce244b5056f49e6cf57afb8bbe5d277ef076bbe1
yu kawase <kawase@kawase-no-MacBook-Air.local>
2013-03-13 23:51:09 +0900

    2

--- cocolife/Mediator.m
+++ cocolife/Mediator.m
@@ -14,6 +14,7 @@
 @implementation Mediator
 
 -(void) initialize{
+    [[UIApplication sharedApplication] setStatusBarHidden:NO];
     loading = NO;
     [ImageCache create];
     [self createController];
---
+++ cocolife/lib/adstir/readme.txt
@@ -0,0 +1,7 @@
+AdStir SDK for iPhone 1.4.0
+
+[ Document for Developer ]
+http://wiki.ad-stir.com
+
+[ Adstir Web Site ]
+http://ad-stir.com
